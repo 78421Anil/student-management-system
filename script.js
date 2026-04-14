62a3a01d7315d120dc1a0306340ab97a778e7e81
78421Anil <95702233+78421Anil@users.noreply.github.com>
2026-04-14 10:50:03 +0530

    Remove login function and log app load

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-function login(username, password) {
-    if(username === "admin" && password === "1234") {
-        console.log("Login Successful");
-    } else {
-        console.log("Invalid Credentials");
-    }
-}
+console.log("App Loaded");
