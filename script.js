5c9566f11ae3334d3dc03991f68ce61c5dbee919
78421Anil <95702233+78421Anil@users.noreply.github.com>
2026-04-14 10:50:41 +0530

    Add login function with credential validation

---
+++ script.js
@@ -0,0 +1,7 @@
+function login(username, password) {
+    if(username === "admin" && password === "1234") {
+        console.log("Login Successful");
+    } else {
+        console.log("Invalid Credentials");
+    }
+}
